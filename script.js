79247630794047f151ee7bcd2cce922e7ad699c2
Akemi <wakamihirro@gmail.com>
2025-04-02 16:34:51 +0700

    2/4/2025 Thêm Banner, Slider

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n){
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n){
+    var i;
+    var x = document.getElementsByClassName("banner-slider");
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    else if (n < 1) {
+        slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display="block";
+}
